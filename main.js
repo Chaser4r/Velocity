7be635a615b190f4d3ce83d793d725d26d8741b7
Chaser4r <iisunargulov@gmail.com>
2025-03-10 13:18:56 +0500

    правка навигации в шапке

--- main.js
+++ main.js
@@ -32,6 +32,9 @@ btns.forEach((btn, btnIndex) => {
 // Anchors
 
 const anchors = document.querySelectorAll('.header__nav a');
+const anchorHome = anchors[0];
+const anchorContacts = anchors[1];
+const footer = document.querySelector('footer');
 
 anchors.forEach(anchor => {
   anchor.addEventListener('click', event => {
@@ -39,6 +42,23 @@ anchors.forEach(anchor => {
     const elem = document.querySelector(id);
 
     event.preventDefault();
-    window.scrollTo(0, elem.offsetTop - 60)
+    window.scrollTo(0, elem.offsetTop - 60);
+
+    // event.target.parentElement.classList.add('header__item_active');
+    // event.target.parentElement.nextElementSibling?.classList.remove('header__item_active');
+    // event.target.parentElement.previousElementSibling?.classList.remove('header__item_active');
   })
+})
+
+document.addEventListener('scroll', event => {
+  if (
+    footer.getBoundingClientRect().y <= 61
+    || footer.getBoundingClientRect().bottom <= document.documentElement.clientHeight
+  ) {
+    anchorHome.parentElement.classList.remove('header__item_active');
+    anchorContacts.parentElement.classList.add('header__item_active')
+  } else {
+    anchorHome.parentElement.classList.add('header__item_active');
+    anchorContacts.parentElement.classList.remove('header__item_active')
+  }
 })
